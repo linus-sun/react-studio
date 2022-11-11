6b996c1ea53024105a2d13fc9864a43704ce70ec
linus-sun <linus.a.sun@gmail.com>
2022-11-11 02:20:17 -0500

    code

--- src/App.js
+++ src/App.js
@@ -8,23 +8,57 @@ bakeryData.forEach((item) => {
 });
 /* ############################################################## */
 
+
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  function addCart(item) {
+    setCart(cart.concat([item]));
+    setTotal(total + item.price);
+  }
+
+  function BakeryItem({ item }) {
+    return (
+      <div class="bakeryItem">
+      <p>{item.name}</p>
+      <img src={item.image} alt={item.name} class="bk-img"></img>
+      <div class="bakeryPrice">
+      <p>${item.price}</p>
+      <button class="cartButton" onClick={() => addCart(item)}>Add to Cart</button>
+      </div>
+      </div>
+    )
+  }
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div class="parent">
+      
 
+      <div class="item-container">
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+          <BakeryItem item={item}/>
+        
+        
       ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
       </div>
-    </div>
+
+      <div class="cart">
+        <h2 class="cart-text">Cart</h2>
+        {cart.map((item, index) => (
+          <p class="cart-item">{item.name} : {item.price}</p>
+        )
+
+        )}
+
+        <h6 class="cart-item">Total Price: {total}</h6>
+      </div>
+      </div>
+      </div>
   );
 }
 
